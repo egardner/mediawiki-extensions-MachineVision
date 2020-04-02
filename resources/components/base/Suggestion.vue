41ef611fd6324051e0750483174b729fc4aa8e12
annet <atomasevich@wikimedia.org>
2020-04-02 09:51:36 -0400

    Simplify transitions
    Change-Id: Ieeaf06f77288778d15e7372a599b2d2090d98308

--- resources/components/base/Suggestion.vue
+++ resources/components/base/Suggestion.vue
@@ -3,14 +3,13 @@
 		class="mw-suggestion"
 		tabindex="0"
 		v-bind:class="classObject"
-		v-on:click="onClick"
+		v-on:click="$emit( 'click' )"
 		v-on:keydown.enter="onClick"
 	>
 		<label class="mw-suggestion__label">
 			<slot />
 		</label>
 		<icon
-			v-if="confirmed"
 			icon="check"
 			v-bind:title="iconText"
 			v-bind:label="iconText"
@@ -41,29 +40,15 @@ module.exports = {
 
 	data: function () {
 		return {
-			iconText: this.$i18n( 'machinevision-suggestion-confirm-undo-title', this.text ).parse(),
-			toggled: false
+			iconText: this.$i18n( 'machinevision-suggestion-confirm-undo-title', this.text ).parse()
 		};
 	},
 
 	computed: {
 		classObject() {
 			return {
-				'mw-suggestion--confirmed': this.confirmed,
-				'mw-suggestion--toggled': this.toggled
+				'mw-suggestion--confirmed': this.confirmed
 			};
-		}
-	},
-
-	methods: {
-		onClick: function () {
-			// Adding the toggled class only after a suggestion has been toggled
-			// once, that is, the first time it is selected, will allow us to
-			// animate the process of un-selecting a suggestion (sliding it back
-			// to center) without firing off that animation when the component
-			// first mounts.
-			this.toggled = true;
-			this.$emit( 'click' );
 		}
 	}
 };
@@ -90,24 +75,20 @@ module.exports = {
 	}
 
 	.mw-suggestion__label {
+		.transition-transform( 0.2s );
 		cursor: pointer;
 		display: inline-block;
 	}
 
-	// Animate the process of moving a label back to center when unconfirmed.
-	&--toggled {
-		.mw-suggestion__label {
-			@keyframes slideRight {
-				0% {
-					.transform( translateX( -0.5em ) );
-				}
-
-				100% {
-					.transform( translateX( 0 ) );
-				}
-			}
-			animation: slideRight 0.2s;
-		}
+	.mw-icon {
+		.transition( opacity 0.2s );
+		min-height: 0;
+		min-width: 0;
+		opacity: 0;
+		position: absolute;
+		right: 0.5em;
+		visibility: hidden; // Hide from screen readers.
+		width: 0; // Don't take up space yet.
 	}
 
 	&--confirmed {
@@ -117,25 +98,12 @@ module.exports = {
 		position: relative;
 
 		.mw-suggestion__label {
-			@keyframes slideLeft {
-				0% {
-					.transform( translateX( 0 ) );
-				}
-
-				100% {
-					.transform( translateX( -0.5em ) );
-				}
-			}
-			.transform( translateX( -0.5em ) );
-			animation: slideLeft 0.2s;
+			transform: translateX( -0.5em );
 		}
 
 		.mw-icon {
-			.fade-in( 0.2s );
-			min-height: 0;
-			min-width: 0;
-			position: absolute;
-			right: 0.5em;
+			opacity: 1;
+			visibility: visible;
 			width: 1em;
 		}
 	}
