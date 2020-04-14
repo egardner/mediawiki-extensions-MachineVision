d38cdffd64a0dee275b6d7f63b3774568f6194a4
Eric Gardner <egardner@wikimedia.org>
2020-04-13 17:43:18 -0700

    Extract fixture data into new file and add tests for ImageCard
    Change-Id: Id4d159bdd2e5b9c61fe8485d687a14ee3cdc1c00

--- tests/jest/CardStack.test.js
+++ tests/jest/CardStack.test.js
@@ -5,6 +5,7 @@ const CardStack = require( '../../resources/components/CardStack.vue' );
 const ImageCard = require( '../../resources/components/ImageCard.vue' );
 const Suggestion = require( '../../resources/components/base/Suggestion.vue' );
 const i18n = require( './plugins/i18n' );
+const imageData = require( './fixtures/imageData.json' );
 
 const localVue = VueTestUtils.createLocalVue();
 localVue.use( i18n );
@@ -20,27 +21,7 @@ describe( 'CardStack', () => {
 		state = {
 			currentTab: 'popular',
 			images: {
-				popular: [
-					{
-						title: 'File:Fullsizeoutput 3c9.jpeg',
-						pageid: 16,
-						descriptionurl: 'http://localhost/mediawiki/index.php/File:Fullsizeoutput_3c9.jpeg',
-						thumburl: 'http://localhost/mediawiki/images/thumb/9/97/Fullsizeoutput_3c9.jpeg/800px-Fullsizeoutput_3c9.jpeg',
-						thumbheight: 533,
-						suggestions: [
-							{
-								text: 'winter',
-								wikidataId: 'Q1311',
-								confirmed: false
-							},
-							{
-								text: 'snow',
-								wikidataId: 'Q7561',
-								confirmed: false
-							}
-						]
-					}
-				],
+				popular: imageData, // contains 4 images
 				user: []
 			},
 			pending: {
@@ -64,7 +45,6 @@ describe( 'CardStack', () => {
 			mutations,
 			actions
 		} );
-
 	} );
 
 	it( 'passes the first image in the appropriate Vuex queue to ImageCard as props', () => {
@@ -92,24 +72,6 @@ describe( 'CardStack', () => {
 		expect( wrapper.contains( ImageCard ) ).toBe( false );
 	} );
 
-	it( 'dispatches the getImages action when the count of the image queue reaches zero', done => {
-		VueTestUtils.shallowMount( CardStack, {
-			propsData: {
-				queue: 'popular'
-			},
-			store,
-			localVue
-		} );
-
-		expect( actions.getImages ).not.toHaveBeenCalled();
-		store.commit( 'removeImage' );
-
-		Vue.nextTick( () => {
-			expect( actions.getImages ).toHaveBeenCalled();
-			done();
-		} );
-	} );
-
 	it( 'Clicking a suggestion inside ImageCard does not change the data inside Cardstack', () => {
 		const wrapper = VueTestUtils.mount( CardStack, {
 			propsData: {
@@ -130,5 +92,26 @@ describe( 'CardStack', () => {
 		suggestion.trigger( 'click' );
 		expect( suggestionInParent.confirmed ).toBe( false );
 		expect( suggestionInChild.confirmed ).toBe( true );
+	} );
+
+	it( 'dispatches the getImages action when the count of the image queue reaches zero', done => {
+		VueTestUtils.shallowMount( CardStack, {
+			propsData: {
+				queue: 'popular'
+			},
+			store,
+			localVue
+		} );
+
+		expect( actions.getImages ).not.toHaveBeenCalled();
+		store.commit( 'removeImage' );
+		store.commit( 'removeImage' );
+		store.commit( 'removeImage' );
+		store.commit( 'removeImage' );
+
+		Vue.nextTick( () => {
+			expect( actions.getImages ).toHaveBeenCalled();
+			done();
+		} );
 	} );
 } );
---
+++ tests/jest/ImageCard.test.js
@@ -0,0 +1,97 @@
+const Vue = require( 'vue' );
+const VueTestUtils = require( '@vue/test-utils' );
+const Vuex = require( 'vuex' );
+const ImageCard = require( '../../resources/components/ImageCard.vue' );
+const Suggestion = require( '../../resources/components/base/Suggestion.vue' );
+const i18n = require( './plugins/i18n' );
+const imageFixtures = require( './fixtures/imageData.json' );
+
+const localVue = VueTestUtils.createLocalVue();
+localVue.use( i18n );
+localVue.use( Vuex );
+
+describe( 'ImageCard', () => {
+	let actions,
+		store;
+
+	beforeEach( () => {
+		actions = {
+			publishTags: jest.fn(),
+			skipImage: jest.fn()
+		};
+
+		store = new Vuex.Store( {
+			state: {},
+			actions
+		} );
+	} );
+
+	it( 'makes a deep copy of suggestions data received from props', () => {
+		const wrapper = VueTestUtils.mount( ImageCard, {
+			propsData: {
+				image: imageFixtures[ 0 ]
+			},
+			store,
+			localVue
+		} );
+
+		let propsSuggestion = imageFixtures[ 0 ].suggestions[ 0 ];
+		let localSuggestion = wrapper.vm.suggestions[ 0 ];
+
+		expect( localSuggestion ).toEqual( propsSuggestion );
+		expect( localSuggestion ).not.toBe( propsSuggestion );
+	} );
+
+	it( 'publish button is disabled when no suggestions are confirmed', () => {
+		const wrapper = VueTestUtils.mount( ImageCard, {
+			propsData: {
+				image: imageFixtures[ 0 ]
+			},
+			store,
+			localVue
+		} );
+
+		const publishButton = wrapper.find( '.wbmad-action-buttons__publish' );
+
+		expect( publishButton.attributes( 'disabled' ) ).toBe( 'disabled' );
+		expect( actions.publishTags ).not.toHaveBeenCalled();
+		publishButton.trigger( 'click' );
+		expect( actions.publishTags ).not.toHaveBeenCalled();
+	} );
+
+	it( 'dispatches the publish action when the publish button is clicked', done => {
+		const wrapper = VueTestUtils.mount( ImageCard, {
+			propsData: {
+				image: imageFixtures[ 0 ]
+			},
+			store,
+			localVue
+		} );
+
+		const publishButton = wrapper.find( '.wbmad-action-buttons__publish' );
+		wrapper.vm.toggleConfirmed( wrapper.vm.suggestions[ 0 ] ); // confirm a suggestion to enable publish button
+
+		Vue.nextTick( () => {
+			expect( actions.publishTags ).not.toHaveBeenCalled();
+			publishButton.trigger( 'click' );
+			expect( actions.publishTags ).toHaveBeenCalled();
+			done();
+		} );
+	} );
+
+	it( 'dispatches the skipImage action when the skip button is clicked', () => {
+		const wrapper = VueTestUtils.mount( ImageCard, {
+			propsData: {
+				image: imageFixtures[ 0 ]
+			},
+			store,
+			localVue
+		} );
+
+		const skipButton = wrapper.find( '.wbmad-action-buttons__skip' );
+
+		expect( actions.skipImage ).not.toHaveBeenCalled();
+		skipButton.trigger( 'click' );
+		expect( actions.skipImage ).toHaveBeenCalled();
+	} );
+} );
