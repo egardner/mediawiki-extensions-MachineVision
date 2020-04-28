8677f99119c9d0a426092973397004e7d7c6f611
Anne Tomasevich <annetvich@gmail.com>
2020-04-28 16:05:20 -0400

    Show pending state on publish (#21)
    * Show pending state on publish
    * Add documentation about possible states and TODO item for future improvement
    
    Co-authored-by: annet <atomasevich@wikimedia.org>
    Co-authored-by: Eric Gardner <gardner.ec@gmail.com>
    Co-authored-by: Eric Gardner <egardner@wikimedia.org>

--- resources/store/actions.js
+++ resources/store/actions.js
@@ -155,6 +155,8 @@ module.exports = {
 				batch: JSON.stringify( reviewBatch )
 			} );
 
+		context.dispatch( 'updatePublishStatus', 'pending' );
+
 		// TODO: this is where we should request more images if we are
 		// running low in a given queue
 
